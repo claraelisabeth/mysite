107dfb72e9113def3cd4ddc687e3a31d807ce7ca
Pichler <clara.pichler@siemens.com>
2024-08-12 16:34:22 +0200

    changes to table.html and creation of custom filter

---
+++ calender/templatetags/custom_filters.py
@@ -0,0 +1,11 @@
+
+from django import template 
+from django.template.defaultfilters import stringfilter
+  
+register = template.Library() 
+  
+@register.filter
+@stringfilter
+def change_to_no(value): 
+    value = "no"
+    return value
